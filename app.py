11046cfe8b7cea6d11dcc73ff5d94d18539e9f7b
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-09-16 15:20:42 +0700

    Change Directory for library.db

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from functools import wraps
 app = Flask(__name__) 
 
 app.config['SECRET_KEY']='Th1s1ss3cr3t'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:/python-projects/rest-api/library.db' 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:/python-projects/library-rest-api/library.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 
 db = SQLAlchemy(app)   
