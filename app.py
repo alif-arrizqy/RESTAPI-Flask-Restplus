60dc2187310778653891e6ad2b45128b8eb4909f
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-09-21 13:20:48 +0700

    update: new table and structure

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import request, jsonify, make_response
+from functools import wraps
+from werkzeug.security import check_password_hash
+import datetime
+import jwt
 from setting import *
-from userModel import *
+from userModel import Users, Authors
 
 def token_required(f):  
     @wraps(f)  
@@ -23,10 +27,9 @@ def token_required(f):
 
 
 @app.route('/register', methods=['POST'])
-def post_signup_user():  
+def signup_user():  
     data = request.get_json()
-    Users.signup_user(data["name"], data["password"])
-
+    Users.signup_user(data["username"], data["password"], data["name"], data["status"])
     return jsonify({'message': 'registered successfully'})
 
 
@@ -37,18 +40,18 @@ def login_user():
     if not auth or not auth.username or not auth.password:
         return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
     
-    user = Users.query.filter_by(name=auth.username).first()
+    user = Users.query.filter_by(username=auth.username).first()
 
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({"public_id": user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config["SECRET_KEY"])
+        token = jwt.encode({"public_id": user.public_id, 'exp': datetime.datetime.now() + datetime.timedelta(minutes=30)}, app.config["SECRET_KEY"])
         return jsonify({"token": token.decode("UTF-8")})
     
     return make_response("Couldnt verify", 401)
 
 
 @app.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
+# @token_required
+def get_all_users():
     return jsonify({"user": Users.get_all_user()})
 
 @app.route('/user/<id>', methods=['DELETE'])
@@ -72,23 +75,20 @@ def get_authors(current_user):
 def add_author(current_user):
     data = request.get_json()
     Authors.add_author(
-        data["name"],
+        data["author"],
         data["book"],
         data["country"],
-        True,
-        current_user.id
-
+        data["publisher"],
+        data["synopsis"],
     )
     
     return jsonify({'message': 'add author successfully'}) 
 
 
-@app.route('/authors/<name>', methods=['DELETE'])
+@app.route('/authors/<author>/<book>', methods=['DELETE'])
 @token_required
-def delete_author(current_user, name):
-    user_id = current_user.id
-    Authors.delete_author(user_id, name)
-    
+def delete_author(author, book):
+    Authors.delete_author(author, book)
     return jsonify({'message': 'Author deleted'})
 
 
--- userModel.py
+++ userModel.py
@@ -1,39 +1,47 @@
+from enum import unique
 from flask import jsonify
 import json
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 import uuid
-import jwt
-import datetime
-from functools import wraps
+from datetime import datetime
+
 from setting import *
 
 db = SQLAlchemy(app)
 
 
 class Users(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.Integer)
-    name = db.Column(db.String(50))
+    username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(50))
-    admin = db.Column(db.Boolean)
+    name = db.Column(db.String(50))
+    status = db.Column(db.Integer)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    # updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)    
+
 
     def json(self):
         return {
-            "id": self.id,
             "public_id": self.public_id,
+            "username": self.username,
             "name": self.name,
-            "admin": self.admin,
+            "status": self.status,
+            "created_on": self.created_on,
+            # "updated_on": self.updated_on,
         }
 
-    def signup_user(_name, _password):
+    def signup_user(_username, _password, _name, _status):
         hashed_password = generate_password_hash(password=_password, method="sha256")
 
         new_user = Users(
             public_id=str(uuid.uuid4()),
-            name=_name,
+            username=_username,
             password=hashed_password,
-            admin=False,
+            name=_name,
+            status=_status,
         )
         db.session.add(new_user)
         db.session.commit()
@@ -44,62 +52,68 @@ class Users(db.Model):
         return [Users.json(user) for user in Users.query.all()]
 
     def delete_user(_id):
-        delete_user = Users.query.filter_by(id=_id).first()
-        db.session.delete(delete_user)
+        Users.query.filter_by(id=_id).delete()
         db.session.commit()
 
     def __repr__(self):
         user_object = {
-            "id": self.id,
             "public_id": self.public_id,
+            "username": self.username,
             "name": self.name,
-            "admin": self.admin,
+            "status": self.status,
+            "created_on": self.created_on,
+            # "updated_on": self.updated_on,
         }
         return json.dumps(user_object)
 
 
 class Authors(db.Model):
+    __tablename__ = "author"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    book = db.Column(db.String(20), unique=True, nullable=False)
+    book = db.Column(db.String(20), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    publisher = db.Column(db.String(30), nullable=False)
     country = db.Column(db.String(50), nullable=False)
-    booker_prize = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer)
+    synopsis = db.Column(db.Text(), nullable=True)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow) 
+
 
     def json(self):
         return {
-            "name": self.name,
             "book": self.book,
+            "author": self.author,
+            "publisher": self.publisher,
             "country": self.country,
-            "book_prize": self.booker_prize,
-            "user_id": self.user_id,
+            "synopsis": self.synopsis,
+            "created_on": self.created_on,
         }
 
     def get_author():
         return [Authors.json(author) for author in Authors.query.all()]
 
-    def add_author(_name, _book, _country, _booker_prize, _user_id):
+    def add_author(_author, _book, _country, _publisher, _synopsis):
         new_author = Authors(
-            name=_name,
+            author=_author,
             book=_book,
             country=_country,
-            booker_prize=_booker_prize,
-            user_id=_user_id,
+            publisher=_publisher,
+            synopsis=_synopsis,
         )
         db.session.add(new_author)
         db.session.commit()
 
-    def delete_author(_user_id, _name):
-        delete_author = Authors.query.filter_by(name=_name, user_id=_user_id).first()
-        db.session.delete(delete_author)
+    def delete_author(_book, _author):
+        Authors.query.filter_by(author=_author, book=_book).delete()
         db.session.commit()
+        
 
     def __repr__(self):
         author_object = {
-            "name": self.name,
+            "author": self.author,
             "book": self.book,
             "country": self.country,
-            "book_prize": self.booker_prize,
-            "user_id": self.user_id,
+            "publisher": self.publisher,
+            "synopsis": self.synopsis,
+            "created_on": self.created_on,
         }
         return json.dumps(author_object)
