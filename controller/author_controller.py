99b90a4766d341def87551fc7b142cfb78a9b387
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-09-30 15:37:01 +0700

    update: documentation is update and handling error

--- controller/author_controller.py
+++ controller/author_controller.py
@@ -6,42 +6,51 @@ def create(_id):
     """
     create author
     """
-    data = Users.query.filter_by(id=_id).first()
-    if not data:
-        return jsonify({"message": "ID is not found"}), 401
+    try:
+        data = Users.query.filter_by(id=_id).first()
+        if not data:
+            return jsonify({"message": "ID is not found"}), 401
 
-    _author_name = request.json["author_name"]
-    _email = request.json["email"]
-    _user_id = data.id
+        _author_name = request.json["author_name"]
+        _email = request.json["email"]
+        _user_id = data.id
 
-    new_author = Author(author_name=_author_name, email=_email, user_id=_user_id)
-    db.session.add(new_author)
-    db.session.commit()
-    return jsonify({"message": "author successfully added"})
+        new_author = Author(author_name=_author_name, email=_email, user_id=_user_id)
+        db.session.add(new_author)
+        db.session.commit()
+        return jsonify({"message": "author successfully added"})
+    except Exception as e:
+        print(e)
 
 
 def get_all_author():
     """
     Return all author
     """
-    return [Author.json(author) for author in Author.query.all()]
+    try:
+        return [Author.json(author) for author in Author.query.all()]
+    except Exception as e:
+        print(e)
 
 
 def get_single_author(_id):
     """
     Return single author
     """
-    data = Author.query.filter_by(id=_id).first()
-    if not data:
-        return jsonify({"message": "ID is not found"}), 401
+    try:
+        data = Author.query.filter_by(id=_id).first()
+        if not data:
+            return jsonify({"message": "ID is not found"}), 401
 
-    get_data = singleTransform(data)
-    return jsonify({"author": get_data})
+        get_data = singleTransform(data)
+        return jsonify({"author": get_data})
+    except Exception as e:
+        print(e)
 
 
 def singleTransform(values):
     """
-    Get data from Author
+    Return value to Author
     """
     data = {
         "id": values.id,
@@ -53,26 +62,39 @@ def singleTransform(values):
 
 
 def createdBy(datas):
+    """
+    Return name value from Users to Author
+    """
     data = {"name": datas.name}
     return data
 
 
 def update(_id):
-    _author_name = request.json["author_name"]
-    _email = request.json["email"]
-    author = Author.query.filter_by(id=_id).first()
-    author.author_name=_author_name
-    author.email=_email
-    db.session.commit()
-    return jsonify({"message": "Successfully update author data!"})
+    """
+    Update name author and email by id
+    """
+    try:
+        _author_name = request.json["author_name"]
+        _email = request.json["email"]
+        author = Author.query.filter_by(id=_id).first()
+        author.author_name = _author_name
+        author.email = _email
+        db.session.commit()
+        return jsonify({"message": "Successfully update author data!"})
+    except Exception as e:
+        print(e)
 
 
 def delete(_id):
-    author = Author.query.filter_by(id=_id).delete()
-    if not author:
-        return jsonify({"message": "ID is not found"}), 401
-    
-    db.session.commit()
-    return jsonify({"message": "Successfully delete author data!"})
+    """
+    Delete author
+    """
+    try:
+        author = Author.query.filter_by(id=_id).delete()
+        if not author:
+            return jsonify({"message": "ID is not found"}), 401
 
-
+        db.session.commit()
+        return jsonify({"message": "Successfully delete author data!"})
+    except Exception as e:
+        print(e)
