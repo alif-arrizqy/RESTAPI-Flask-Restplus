4832bbd28ef56ce96a462780ee02dc2623749468
alif-arrizqy <alif.arrizqy20@gmail.com>
2021-10-01 11:00:35 +0700

    update: create method for book model and modified key for json

--- model/models.py
+++ model/models.py
@@ -77,10 +77,11 @@ class Author(db.Model):
     email = db.Column(db.String(20))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
-
+    get_book = db.relationship("Book", backref="author")
 
     def json(self):
         return {
+            "id_author": self.id,
             "author_name": self.author_name,
             "email": self.email,
             "user_id": self.user_id,
@@ -89,9 +90,44 @@ class Author(db.Model):
 
     def __repr__(self):
         author_object = {
+            "id_author": self.id,
             "author_name": self.author_name,
             "email": self.email,
             "user_id": self.user_id,
             "created_on": self.created_on,
         }
         return json.dumps(author_object)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title_book = db.Column(db.String(50))
+    publisher = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    synopsis = db.Column(db.String(255))
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def json(self):
+        return {
+            "id_book": self.id,
+            "title_book": self.title_book,
+            "publisher": self.publisher,
+            "country": self.country,
+            "synopsis": self.synopsis,
+            "created_on": self.created_on,
+            "updated_on": self.updated_on,
+        }
+
+    def __repr__(self):
+        book_object = {
+            "id_book": self.id,
+            "title_book": self.title_book,
+            "publisher": self.publisher,
+            "country": self.country,
+            "synopsis": self.synopsis,
+            "created_on": self.created_on,
+            "updated_on": self.updated_on,
+        }
+        return json.dumps(book_object)
